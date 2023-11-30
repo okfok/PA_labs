9c0dd9d8e18ad76c053ffd7476c8464a8bd3e338
okf0k <lubomvis@gmail.com>
2023-11-30 16:09:13 +0200

    upgrade work with files

--- core.py
+++ core.py
@@ -1,10 +1,50 @@
 import json
 import os
+import random
 from pathlib import Path
 
 from pydantic import BaseModel, Field
 
 T = 2
+
+CONF_FILE_NAME = 'names.conf'
+NAME_PATH = 'nodes/node-'
+NAME_CHARS = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+
+
+class NodeNameConf(BaseModel):
+    root: str
+    used_names: list[str] = Field(default_factory=list)
+
+    def __init__(self):
+
+        if Path(CONF_FILE_NAME).is_file():
+            with open(CONF_FILE_NAME, 'r') as file:
+                raw_data = file.read()
+                data = json.loads(raw_data)
+                super().__init__(**data)
+            os.remove(CONF_FILE_NAME)
+        else:
+            super().__init__(root=NodeNameConf._rand_name())
+            self.used_names.append(self.root)
+
+    def save(self):
+        with open(CONF_FILE_NAME, 'w') as file:
+            file.write(self.model_dump_json())
+
+    @staticmethod
+    def _rand_name():
+        return NAME_PATH + ''.join(random.choice(NAME_CHARS) for _ in range(50))
+
+    def get_unique_name(self):
+        name = NodeNameConf._rand_name()
+        while name in self.used_names:
+            name = NodeNameConf._rand_name()
+        self.used_names.append(name)
+        return name
+
+
+name_conf = NodeNameConf()
 
 
 class Node(BaseModel):
@@ -15,7 +55,7 @@ class Node(BaseModel):
 
     def __init__(self, file_name: str = None):
         if file_name is None:
-            file_name = 'node'
+            file_name = name_conf.get_unique_name()
 
         if Path(file_name).is_file():
             with open(file_name, 'r') as file:
@@ -44,14 +84,6 @@ class Node(BaseModel):
     def leaf(self):
         return len(self.node_refs) == 0
 
-    def rename(self, fn: str = None):
-        if fn is None:
-            fn = self.name
-        for i in range(len(self.node_refs)):
-            Node(self.node_refs[i]).rename(fn + f'-{i}')
-            self.node_refs[i] = fn + f'-{i}'
-        self.name = fn
-
     def insert_not_full(self, k):
         i = self.size - 1
         if self.leaf:
@@ -64,7 +96,7 @@ class Node(BaseModel):
         else:
             while i >= 0 and self.keys[i] > k:
                 i -= 1
-            if (child := Node(self.node_refs[i+1])).is_full:
+            if (child := Node(self.node_refs[i + 1])).is_full:
                 self.splitChild(i + 1, child)
                 if self.keys[i + 1] < k:
                     i += 1
@@ -91,14 +123,17 @@ class Tree:
     node: Node
 
     def __init__(self):
-        self.root = Node()
+        self.root = Node(name_conf.root)
 
     def insert(self, key, val):
         if self.root.is_full:
-            temp = Node("temp")
+            root = self.root
+            temp = Node()
             self.root = temp
-            temp.node_refs.insert(0, 'node')
-            temp.rename('node')
+            temp.node_refs.insert(0, root.name)
+
+            name_conf.root = self.root.name
+
             self.split_child(temp, 0)
             self.insert_non_full(temp, key, val)
         else:
@@ -106,12 +141,8 @@ class Tree:
 
     @staticmethod
     def split_child(x: Node, i):
-        z = Node("temp")
+        z = Node()
         x.node_refs.insert(i + 1, z.name)
-
-        del z
-
-        x.rename()
 
         y = Node(x.node_refs[i])
         z = Node(x.node_refs[i + 1])
@@ -143,3 +174,13 @@ class Tree:
             self.insert_non_full(child_i, key, val)
 
 
+def clean():
+    os.remove(CONF_FILE_NAME)
+    folder = 'nodes/'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
