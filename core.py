37779c928b85996992d0cebaeda4c3a7134d8c40
okf0k <lubomvis@gmail.com>
2023-12-30 22:08:57 +0200

    algorithm update

--- core.py
+++ core.py
@@ -8,7 +8,9 @@ GRAPH_SIZE = 300
 EDGE_PROBABILITY = 0.05
 
 POPULATION_SIZE = 10
-MUTATION_PROBABILITY = 0.025
+CROSSOVER_COUNT = 1
+MUTATION_PROBABILITY = 0.005
+MUTATION_MAX_COUNT = 10
 
 
 class Solution:
@@ -17,13 +19,13 @@ class Solution:
         self.genes = genes or [False for _ in range(GRAPH_SIZE)]
 
     @property
-    def cover_len(self):
+    def cover_len(self) -> int:
         return sum(self.genes)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Sol({self.cover_len})'
 
-    def draw(self):
+    def draw(self) -> None:
         plt.figure(figsize=(32, 24))
         nx.draw_networkx(self.graph, node_color=['blue' if gene else 'white' for gene in self.genes], with_labels=True)
         plt.show()
@@ -51,7 +53,19 @@ class Solution:
 
         return self
 
-    def local_improvement(self):
+    @property
+    def is_solved(self) -> bool:
+        edges = list(self.graph.edges)
+
+        for i in range(GRAPH_SIZE):
+            if self.genes[i]:
+                for j in range(len(edges) - 1, -1, -1):
+                    if i in edges[j]:
+                        edges.pop(j)
+
+        return len(edges) == 0
+
+    def local_improvement(self) -> 'Solution':
         edges = list(self.graph.edges)
         covered_edges = set()
         for i in random.sample(range(GRAPH_SIZE), GRAPH_SIZE):
@@ -66,19 +80,49 @@ class Solution:
             if not_covering:
                 self.genes[i] = False
 
-        assert len(edges) == len(covered_edges)
+        assert self.is_solved
+        return self
 
-    def mutate(self):
+    def mutate_prb(self) -> 'Solution':
         for i in range(GRAPH_SIZE):
             self.genes[i] = bool(self.genes[i] - (random.randint(0, 1000) < 1000 * MUTATION_PROBABILITY))
 
-        return self.solve()
+        return self
+
+    def mutate_max(self) -> 'Solution':
+        indexes_to_mutate = random.sample(range(GRAPH_SIZE), random.randint(1, MUTATION_MAX_COUNT))
+        for i in indexes_to_mutate:
+            self.genes[i] = bool(self.genes[i] - True)
+
+        return self
 
     @classmethod
-    def crossover_prb(cls, first: 'Solution', second: 'Solution'):
+    def crossover_prb(cls, first: 'Solution', second: 'Solution') -> list['Solution']:
         new_genes = [random.choice((first.genes[i], second.genes[i])) for i in range(GRAPH_SIZE)]
 
-        return cls(first.graph, new_genes)
+        return [cls(first.graph, new_genes)]
+
+    @classmethod
+    def crossover_one_point(cls, first: 'Solution', second: 'Solution') -> list['Solution']:
+        point = random.randint(1, GRAPH_SIZE - 1)
+
+        return [
+            cls(first.graph, first.genes[:point] + second.genes[point:]),
+            cls(first.graph, second.genes[:point] + first.genes[point:])
+        ]
+
+    @classmethod
+    def crossover_two_point(cls, first: 'Solution', second: 'Solution') -> list['Solution']:
+        point1 = random.randint(1, GRAPH_SIZE // 2)
+        point2 = random.randint(GRAPH_SIZE // 2 + 1, GRAPH_SIZE - 1)
+
+        return [
+            cls(first.graph, first.genes[:point1] + second.genes[point1:point2] + first.genes[point2:]),
+            cls(first.graph, second.genes[:point1] + first.genes[point1:point2] + second.genes[point2:])
+        ]
+
+    def copy(self):
+        return self.__class__(self.graph, self.genes.copy())
 
 
 class Population:
@@ -93,13 +137,27 @@ class Population:
         parrent1, parrent2 = random.choices(
             self.population, [1 / solution.cover_len for solution in self.population], k=2
         )
-        children: list[Solution] = [Solution.crossover_prb(parrent1, parrent2) for _ in range(POPULATION_SIZE // 2)]
+        children: list[Solution] = [
+            child for child in
+            Solution.crossover_two_point(parrent1, parrent2) for _ in range(CROSSOVER_COUNT)  # crossover
+            if child.is_solved
+        ]
 
-        for child in children:
-            child.mutate()
-            child.local_improvement()
+        if len(children):
+            self.population += [child.copy() for child in children]
 
-        self.population = children + self.population
+            mutated_children = list(
+                filter(lambda x: x.is_solved, [child.mutate_max() for child in children]))  # mutation
+            if len(mutated_children):
+                self.population += [child.copy() for child in mutated_children]
+
+                improved_children = list(
+                    filter(lambda x: x.is_solved,
+                           [child.local_improvement() for child in mutated_children]))  # improvement
+                if len(improved_children):
+                    self.population += improved_children
+
+        self.population = random.sample(self.population, len(self.population))
 
         self.population.sort(key=lambda x: x.cover_len)
 
@@ -120,7 +178,7 @@ ITERATION_COUNT = 1000
 
 def _main():
     pop = Population()
-    pop.draw()
+    # pop.draw()
     y = []
     for _ in range(ITERATION_COUNT):
         y.append(pop.best.cover_len)
@@ -129,8 +187,13 @@ def _main():
     plt.plot(x, y)
     plt.show()
     print(y[0] - y[-1])
-    pop.best.draw()
+    # pop.best.draw()
+    return y[0] - y[-1]
+
+
+def _test():
+    print(sum([_main() for _ in range(5)]) / 5)
 
 
 if __name__ == '__main__':
-    _main()
+    _test()
